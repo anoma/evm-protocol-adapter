e5f1a6f17254c822f474a2adaed9ba0531895072
Michael Heuer <michael.heuer@rwth-aachen.de>
2025-01-09 17:59:49 +0100

    feat: add wrapper base contract

--- src/ERC20ResourceWrapper.sol
+++ src/ERC20ResourceWrapper.sol
@@ -4,53 +4,49 @@ pragma solidity >=0.8.25;
 import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-import { IResourceWrapper } from "./interfaces/IResourceWrapper.sol";
+import { ResourceWrapperBase } from "./ResourceWrapperBase.sol";
 import { Resource, Map } from "./Types.sol";
 
-contract ERC20ResourceWrapper is IResourceWrapper, Ownable {
+contract ERC20ResourceWrapper is Ownable, ResourceWrapperBase {
     using Map for Map.KeyValuePair[];
 
-    bytes32 internal immutable RESOURCE_KIND;
     IERC20 internal immutable TOKEN_CONTRACT;
 
-    constructor(address protocolAdapter, bytes32 resourceKind, IERC20 erc20) Ownable(protocolAdapter) {
+    constructor(
+        address protocolAdapter,
+        bytes32 resourceKind,
+        IERC20 erc20
+    )
+        ResourceWrapperBase(protocolAdapter, resourceKind)
+    {
         TOKEN_CONTRACT = erc20;
-        RESOURCE_KIND = resourceKind;
     }
 
-    function kind() external view returns (bytes32) {
-        return RESOURCE_KIND;
-    }
-
-    function wrap(
-        bytes32 nullifier,
+    function _wrap(
+        bytes32, // nullifier, // NOTE: unused
         Resource calldata resource,
         Map.KeyValuePair[] calldata appData
     )
-        external
-        onlyOwner
+        internal
+        override
     {
         bytes memory value = appData.lookup(resource.valueRef);
         address _owner = abi.decode(value, (address));
 
         TOKEN_CONTRACT.transferFrom({ from: _owner, to: address(this), value: resource.quantity });
-
-        emit ResourceWrapped(nullifier, resource);
     }
 
-    function unwrap(
-        bytes32 commitment,
+    function _unwrap(
+        bytes32, //commitment, // NOTE: unused
         Resource calldata resource,
         Map.KeyValuePair[] calldata appData
     )
-        external
-        onlyOwner
+        internal
+        override
     {
         bytes memory value = appData.lookup(resource.valueRef);
         address _owner = abi.decode(value, (address));
 
         TOKEN_CONTRACT.transfer({ to: _owner, value: resource.quantity });
-
-        emit ResourceUnwrapped(commitment, resource);
     }
 }
