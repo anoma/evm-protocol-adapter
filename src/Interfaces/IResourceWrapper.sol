afa60eaa66bb8a2810a924e3318d703024ef755c
Michael Heuer <michael.heuer@rwth-aachen.de>
2025-01-09 17:47:12 +0100

    docs: fix typo in comment

--- src/Interfaces/IResourceWrapper.sol
+++ src/Interfaces/IResourceWrapper.sol
@@ -4,12 +4,13 @@ pragma solidity >=0.8.25;
 import { Resource, Map } from "../Types.sol";
 
 interface IResourceWrapper {
-    event ResourceWrapped(bytes32 indexed nullifier, Resource resource); // TOOD Does indexed make sense?
+    event ResourceWrapped(bytes32 indexed nullifier, Resource resource); // TODO Does indexed make sense?
+
     event ResourceUnwrapped(bytes32 indexed commitment, Resource resource);
+
+    function kind() external view returns (bytes32);
 
     function wrap(bytes32 nullifier, Resource calldata resource, Map.KeyValuePair[] calldata appData) external;
 
     function unwrap(bytes32 commitment, Resource calldata resource, Map.KeyValuePair[] calldata appData) external;
-
-    function kind() external view returns (bytes32);
 }
