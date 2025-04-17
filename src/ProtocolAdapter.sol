17e1c3828962aec217bdf38d35fdd8a8d653a885
Michael Heuer <michael.heuer@rwth-aachen.de>
2025-04-17 09:44:10 +0200

    docs: satisfy linter

--- src/ProtocolAdapter.sol
+++ src/ProtocolAdapter.sol
@@ -128,12 +128,13 @@ contract ProtocolAdapter is
     }
 
     // solhint-disable-next-line function-max-lines
+    // slither-disable-next-line calls-loop
     function _verify(Transaction calldata transaction) internal view {
         // Can also be named DeltaHash (which is what Yulia does).
         uint256[2] memory transactionDelta = Delta.zero();
 
         // Helper variable
-        uint256 resourceCount;
+        uint256 resourceCount = 0;
 
         uint256 nActions = transaction.actions.length;
         for (uint256 i; i < nActions; ++i) {
@@ -254,6 +255,7 @@ contract ProtocolAdapter is
         */
     }
 
+    // slither-disable-next-line calls-loop
     function _verifyForwarderCalls(Action calldata action) internal view {
         uint256 len = action.resourceCalldataPairs.length;
         for (uint256 j; j < len; ++j) {
