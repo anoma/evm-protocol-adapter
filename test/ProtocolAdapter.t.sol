b9f597b3be5485d6093a950218ba4e2cf19ff18b
Michael Heuer <michael.heuer@rwth-aachen.de>
2025-05-20 22:15:36 +0200

    WIP: verification fails

--- test/ProtocolAdapter.t.sol
+++ test/ProtocolAdapter.t.sol
@@ -1,22 +1,19 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.27;
 
-import {RiscZeroVerifierRouter} from "@risc0-ethereum/RiscZeroVerifierRouter.sol";
+import {IRiscZeroVerifier, VerificationFailed} from "@risc0-ethereum/IRiscZeroVerifier.sol";
+
 import {Test} from "forge-std/Test.sol";
 
 import {ProtocolAdapter} from "../src/ProtocolAdapter.sol";
 import {Example} from "./mocks/Example.sol";
 
 contract ProtocolAdapterTest is Test {
-    uint8 internal constant _COMMITMENT_TREE_DEPTH = 32;
+    IRiscZeroVerifier internal _sepoliaVerifier;
+    bytes32 internal _complianceCircuitID;
+    uint8 internal _commitmentTreeDepth;
 
-    uint8 internal constant _ACTION_TREE_DEPTH = 4;
-
-    RiscZeroVerifierRouter internal constant _SEPOLIA_VERIFIER =
-        RiscZeroVerifierRouter(address(0x925d8331ddc0a1F0d96E68CF073DFE1d92b69187));
-
-    bytes32 internal constant _COMPLIANCE_CIRCUIT_ID =
-        0xbf165219f3e8390e1fca936de31db398b66efaef8af1b836f23c271b7ef66103;
+    uint8 internal _actionTreeDepth;
 
     ProtocolAdapter internal _pa;
 
@@ -24,16 +21,28 @@ contract ProtocolAdapterTest is Test {
         // Fork Sepolia
         vm.selectFork(vm.createFork("sepolia"));
 
+        string memory path = "./script/constructor-args.txt";
+
+        _sepoliaVerifier = IRiscZeroVerifier(vm.parseAddress(vm.readLine(path)));
+        _complianceCircuitID = vm.parseBytes32(vm.readLine(path));
+        _commitmentTreeDepth = uint8(vm.parseUint(vm.readLine(path)));
+        _actionTreeDepth = uint8(vm.parseUint(vm.readLine(path)));
+
         _pa = new ProtocolAdapter({
-            riscZeroVerifier: _SEPOLIA_VERIFIER,
-            complianceCircuitID: _COMPLIANCE_CIRCUIT_ID,
-            commitmentTreeDepth: _COMMITMENT_TREE_DEPTH,
-            actionTreeDepth: _ACTION_TREE_DEPTH
+            riscZeroVerifier: _sepoliaVerifier,
+            complianceCircuitID: _complianceCircuitID,
+            commitmentTreeDepth: _commitmentTreeDepth,
+            actionTreeDepth: _actionTreeDepth
         });
     }
 
-    function test_sel() public {
-        _pa.execute(Example.transaction());
+    function test_verify() public {
+        vm.expectRevert(VerificationFailed.selector);
+
+        _pa.verify(Example.transaction());
+
+        // solhint-disable-next-line gas-custom-errors
+        revert("The above should NOT revert");
     }
 
     function test_execute() public {
@@ -49,34 +58,5 @@ contract ProtocolAdapterTest is Test {
         });
 
         _pa.verify(txn);
-    }*/
-
-    function test_verify_tx() public view {
-        _pa.verify(Example.transaction());
-    }
-
-    /*
-    function test_benchmark() public {
-        uint16[3] memory n = [uint16(5), uint16(50), uint16(500)];
-
-        for (uint256 i = 0; i < n.length; ++i) {
-            (Resource[] memory consumed, Resource[] memory created) = MockTypes.mockResources({
-                nConsumed: n[i],
-                ephConsumed: true,
-                nCreated: n[i],
-                ephCreated: false,
-                seed: i
-            });
-
-            Transaction memory txn = MockTypes.mockTransaction({
-                mockVerifier: _mockVerifier,
-                root: _pa.latestRoot(),
-                consumed: consumed,
-                created: created
-            });
-
-            _pa.verify(txn);
-            _pa.execute(txn);
-        }
     }*/
 }
