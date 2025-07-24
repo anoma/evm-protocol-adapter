02654e123bb233ecd47b275fbe12181f7624561a
Artem Gureev <67437331+agureev@users.noreply.github.com>
2025-07-24 15:48:19 +0400

    Remove Unnecessary Variable
    Co-authored-by: Michael <20623991+heueristik@users.noreply.github.com>
    
    Co-authored-by: Michael <20623991+heueristik@users.noreply.github.com>

--- contracts/test/Deployment.t.sol
+++ contracts/test/Deployment.t.sol
@@ -5,14 +5,10 @@ import {Test} from "forge-std/Test.sol";
 
 import {Deploy} from "../script/Deploy.s.sol";
 
-import {ProtocolAdapter} from "../src/ProtocolAdapter.sol";
-
 contract ProtocolAdapterTest is Test {
-    ProtocolAdapter internal _pa;
-
     function test_deploy() public {
         Deploy deployScript = new Deploy();
 
-        _pa = ProtocolAdapter(deployScript.run());
+        deployScript.run();
     }
 }
