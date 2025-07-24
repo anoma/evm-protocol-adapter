901b839c5e44d81ba243663069b2a93eaa21d959
Michael Heuer <michael.heuer@rwth-aachen.de>
2025-07-24 10:11:46 +0200

    refactor: move sepolia fork selection and protocol adapter creation into helper function

--- contracts/test/ProtocolAdapter.t.sol
+++ contracts/test/ProtocolAdapter.t.sol
@@ -12,8 +12,6 @@ import {Transaction, Action} from "../src/Types.sol";
 import {Example} from "./mocks/Example.sol";
 
 contract ProtocolAdapterTest is Test {
-    ProtocolAdapter internal _pa;
-
     RiscZeroVerifierRouter internal _sepoliaVerifierRouter;
 
     function setUp() public {
@@ -23,75 +21,60 @@ contract ProtocolAdapterTest is Test {
         string memory path = "./script/constructor-args.txt";
 
         _sepoliaVerifierRouter = RiscZeroVerifierRouter(vm.parseAddress(vm.readLine(path)));
-
-        _pa = new ProtocolAdapter({
-            riscZeroVerifierRouter: RiscZeroVerifierRouter(_sepoliaVerifierRouter), // Sepolia verifier
-            commitmentTreeDepth: uint8(vm.parseUint(vm.readLine(path))),
-            actionTagTreeDepth: uint8(vm.parseUint(vm.readLine(path)))
-        });
     }
 
     function test_execute() public {
+        ProtocolAdapter pa = _sepoliaProtocolAdapter({forkBeforeRisc0Vulnerability: false});
+
         address riscZeroEmergencyStop =
             address(_sepoliaVerifierRouter.getVerifier(bytes4(Example._CONSUMED_LOGIC_PROOF)));
 
         vm.expectRevert(Pausable.EnforcedPause.selector, riscZeroEmergencyStop);
 
-        _pa.execute(Example.transaction());
+        pa.execute(Example.transaction());
     }
 
     function test_execute_empty_tx() public {
+        ProtocolAdapter pa = _sepoliaProtocolAdapter({forkBeforeRisc0Vulnerability: false});
+
         Transaction memory txn = Transaction({actions: new Action[](0), deltaProof: ""});
-        _pa.execute(txn);
+        pa.execute(txn);
     }
 
     function test_verify() public {
+        ProtocolAdapter pa = _sepoliaProtocolAdapter({forkBeforeRisc0Vulnerability: false});
+
         address riscZeroEmergencyStop =
             address(_sepoliaVerifierRouter.getVerifier(bytes4(Example._CONSUMED_LOGIC_PROOF)));
 
         vm.expectRevert(Pausable.EnforcedPause.selector, riscZeroEmergencyStop);
-        _pa.verify(Example.transaction());
+        pa.verify(Example.transaction());
     }
 
     function test_execute_emits_the_Blob_event() public {
-        /* NOTE:
-         * Here we fork sepolia before emergency stop was called on the `RiscZeroVerifierEmergencyStop` contract for v2.0.0
-         * See
-         * - https://sepolia.etherscan.io/address/0x8A8023bf44CABa343CEef3b06A4639fc8EBeE629
-         * - https://github.com/risc0/risc0/security/advisories/GHSA-g3qg-6746-3mg9
-         * for the details.
-         */
-        vm.selectFork(vm.createFork("sepolia", 8577299 - 1));
-
-        string memory path = "./script/constructor-args.txt";
-
-        _sepoliaVerifierRouter = RiscZeroVerifierRouter(vm.parseAddress(vm.readLine(path)));
-
-        _pa = new ProtocolAdapter({
-            riscZeroVerifierRouter: RiscZeroVerifierRouter(_sepoliaVerifierRouter), // Sepolia verifier
-            commitmentTreeDepth: uint8(vm.parseUint(vm.readLine(path))),
-            actionTagTreeDepth: uint8(vm.parseUint(vm.readLine(path)))
-        });
+        ProtocolAdapter pa = _sepoliaProtocolAdapter({forkBeforeRisc0Vulnerability: true});
 
         Transaction memory txn = Example.transaction();
-        vm.expectEmit(address(_pa));
+        vm.expectEmit(address(pa));
         emit IProtocolAdapter.Blob(txn.actions[0].logicVerifierInputs[0].instance.appData[0]);
 
-        vm.expectEmit(address(_pa));
+        vm.expectEmit(address(pa));
         emit IProtocolAdapter.Blob(txn.actions[0].logicVerifierInputs[0].instance.appData[1]);
 
-        vm.expectEmit(address(_pa));
+        vm.expectEmit(address(pa));
         emit IProtocolAdapter.Blob(txn.actions[0].logicVerifierInputs[1].instance.appData[0]);
 
-        vm.expectEmit(address(_pa));
+        vm.expectEmit(address(pa));
         emit IProtocolAdapter.Blob(txn.actions[0].logicVerifierInputs[1].instance.appData[1]);
 
-        _pa.execute(txn);
+        pa.execute(txn);
     }
 
-    function test_verify_empty_tx() public view {
+    function test_verify_empty_tx() public {
+        ProtocolAdapter pa = _sepoliaProtocolAdapter({forkBeforeRisc0Vulnerability: true});
+
         Transaction memory txn = Transaction({actions: new Action[](0), deltaProof: ""});
-        _pa.verify(txn);
+        pa.verify(txn);
     }
 
     function test_tx_with_cu_mismatch_fails() public view {
@@ -100,9 +83,34 @@ contract ProtocolAdapterTest is Test {
         true;
     }
 
-    function test_verify_reverts_on_action_with_repeating_nullifiers() public view {
+    function test_verify_reverts_on_action_with_repeating_nullifiers() public {
         // TODO: create a transaction with repeating actions (specifically nullifier)
         //       and expect it to revert on appropriate error
         true;
+    }
+
+    function _sepoliaProtocolAdapter(bool forkBeforeRisc0Vulnerability) internal returns (ProtocolAdapter pa) {
+        if (forkBeforeRisc0Vulnerability) {
+            /* NOTE:
+             * Here we fork sepolia before emergency stop was called on the `RiscZeroVerifierEmergencyStop` contract for v2.0.0
+             * See
+             * - https://sepolia.etherscan.io/address/0x8A8023bf44CABa343CEef3b06A4639fc8EBeE629
+             * - https://github.com/risc0/risc0/security/advisories/GHSA-g3qg-6746-3mg9
+             * for the details.
+             */
+            vm.selectFork(vm.createFork("sepolia", 8577299 - 1));
+        } else {
+            vm.selectFork(vm.createFork("sepolia"));
+        }
+
+        string memory path = "./script/constructor-args.txt";
+
+        _sepoliaVerifierRouter = RiscZeroVerifierRouter(vm.parseAddress(vm.readLine(path)));
+
+        pa = new ProtocolAdapter({
+            riscZeroVerifierRouter: RiscZeroVerifierRouter(_sepoliaVerifierRouter), // Sepolia verifier
+            commitmentTreeDepth: uint8(vm.parseUint(vm.readLine(path))),
+            actionTagTreeDepth: uint8(vm.parseUint(vm.readLine(path)))
+        });
     }
 }
