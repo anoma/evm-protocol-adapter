669f36698b9e3c1c88a4df8d0d67ae593ff8725f
Michael Heuer <michael.heuer@rwth-aachen.de>
2025-08-01 12:28:29 +0200

    refactor: simplify deploy scripts

--- contracts/script/DeployERC20Forwarder.sol
+++ contracts/script/DeployERC20Forwarder.sol
@@ -1,23 +1,24 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.30;
 
+import {Script} from "forge-std/Script.sol";
 import {ERC20Forwarder} from "../src/forwarders/ERC20Forwarder.sol";
 import {ProtocolAdapter} from "../src/ProtocolAdapter.sol";
 
-import {BaseScript} from "./Base.s.sol";
-
-contract DeployERC20Wrapper is BaseScript {
+contract DeployERC20Wrapper is Script {
     ProtocolAdapter internal constant _PROTOCOL_ADAPTER = ProtocolAdapter(address(0));
 
     address internal constant _ERC20 = address(0x1111111111111111111111111111111111111111);
 
     bytes32 internal constant _CALLDATA_CARRIER_LOGIC_REF = bytes32(0);
 
-    function run() public broadcast {
+    function run() public {
+        vm.startBroadcast();
         new ERC20Forwarder{salt: sha256("ERC20ForwarderExample")}({
             protocolAdapter: address(_PROTOCOL_ADAPTER),
             erc20: _ERC20,
             calldataCarrierLogicRef: _CALLDATA_CARRIER_LOGIC_REF
         });
+        vm.stopBroadcast();
     }
 }
--- contracts/script/ExecuteExampleTx.s.sol
+++ contracts/script/ExecuteExampleTx.s.sol
@@ -1,13 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.30;
 
+import {Script} from "forge-std/Script.sol";
+
 import {ProtocolAdapter} from "../src/ProtocolAdapter.sol";
 import {Example} from "../test/mocks/Example.sol";
-import {BaseScript} from "./Base.s.sol";
 
-contract ExecuteExampleTx is BaseScript {
-    function run() public broadcast {
-        ProtocolAdapter pa = ProtocolAdapter(0xC5033726a1fb969743A6f5Baf1753D56c6e1692b);
-        pa.execute(Example.transaction());
+contract ExecuteExampleTx is Script {
+    function run() public {
+        vm.startBroadcast();
+        ProtocolAdapter(0xC5033726a1fb969743A6f5Baf1753D56c6e1692b).execute(Example.transaction());
+        vm.stopBroadcast();
     }
 }
