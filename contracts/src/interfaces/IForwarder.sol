7e5714d8b18619dbab6d75bf0cade2508826d0e3
Michael Heuer <michael.heuer@rwth-aachen.de>
2025-08-01 17:41:24 +0200

    doc: fix typos

--- contracts/src/interfaces/IForwarder.sol
+++ contracts/src/interfaces/IForwarder.sol
@@ -6,19 +6,19 @@ pragma solidity ^0.8.30;
 /// @notice The interface of the forwarder base contract.
 /// @custom:security-contact security@anoma.foundation
 interface IForwarder {
-    /// @notice Forwards an external call to read or write EVM state. This function can be only called by the
+    /// @notice Forwards an external call to read or write EVM state. This function can only be called by the
     /// protocol adapter contract.
     /// @param input The `bytes` encoded calldata (including the `bytes4` function selector).
     /// @return output The `bytes` encoded output of the call.
     function forwardCall(bytes memory input) external returns (bytes memory output);
 
-    /// @notice Forwards an external call to read or write EVM state. This function can be only called by the address
+    /// @notice Forwards an external call to read or write EVM state. This function can only be called by the address
     /// set by emergency committee if the RISC Zero emergency stop is active.
     /// @param input The `bytes` encoded calldata (including the `bytes4` function selector).
     /// @return output The `bytes` encoded output of the call.
     function forwardEmergencyCall(bytes memory input) external returns (bytes memory output);
 
-    /// @notice Sets the emergency caller. This function can be only called by the address
+    /// @notice Sets the emergency caller. This function can only be called by the address
     /// set by emergency committee if the RISC Zero emergency stop is active.
     /// @param newEmergencyCaller The emergency caller to set.
     function setEmergencyCaller(address newEmergencyCaller) external;
