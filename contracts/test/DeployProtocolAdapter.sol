ec080df70b29e7e2659ca1fe4d4833dcd5f460f3
Michael Heuer <michael.heuer@rwth-aachen.de>
2025-08-01 17:56:00 +0200

    refactor: test name

--- contracts/test/DeployProtocolAdapter.sol
+++ contracts/test/DeployProtocolAdapter.sol
@@ -6,7 +6,7 @@ import {Test} from "forge-std/Test.sol";
 import {DeployProtocolAdapter} from "../script/DeployProtocolAdapter.s.sol";
 
 contract DeployProtocolAdapterTest is Test {
-    function test_deploy() public {
+    function test_DeployProtocolAdapter() public {
         DeployProtocolAdapter deployScript = new DeployProtocolAdapter();
 
         vm.expectRevert(); // TODO! Remove this when the RISC Zero version has been updated.
