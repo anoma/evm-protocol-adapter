e48218903a20e6f7ad238e8a448af0c32ec95c98
Michael Heuer <michael.heuer@rwth-aachen.de>
2025-08-01 17:44:02 +0200

    refactor: rename files

--- contracts/test/DeployProtocolAdapter.sol
+++ contracts/test/DeployProtocolAdapter.sol
@@ -3,11 +3,11 @@ pragma solidity ^0.8.30;
 
 import {Test} from "forge-std/Test.sol";
 
-import {Deploy} from "../script/Deploy.s.sol";
+import {DeployProtocolAdapter} from "../script/DeployProtocolAdapter.s.sol";
 
-contract DeployTest is Test {
+contract DeployProtocolAdapterTest is Test {
     function test_deploy() public {
-        Deploy deployScript = new Deploy();
+        DeployProtocolAdapter deployScript = new DeployProtocolAdapter();
 
         vm.expectRevert(); // TODO! Remove this when the RISC Zero version has been updated.
         deployScript.run();
