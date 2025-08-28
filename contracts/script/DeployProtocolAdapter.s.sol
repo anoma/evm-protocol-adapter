ef4b02bfa21d6dbea06847ee2b319b828b58d204
Michael Heuer <michael.heuer@rwth-aachen.de>
2025-08-28 16:47:56 +0200

    style: fix linter warnings

--- contracts/script/DeployProtocolAdapter.s.sol
+++ contracts/script/DeployProtocolAdapter.s.sol
@@ -8,24 +8,24 @@ import {Script} from "forge-std/Script.sol";
 import {ProtocolAdapter} from "../src/ProtocolAdapter.sol";
 
 contract DeployProtocolAdapter is Script {
-    mapping(uint256 chainId => string network) networks;
-    mapping(string network => RiscZeroVerifierRouter router) routers;
+    mapping(uint256 chainId => string network) internal _networks;
+    mapping(string network => RiscZeroVerifierRouter router) internal _routers;
 
     constructor() {
-        networks[11155111] = "sepolia";
-        networks[421614] = "arbitrum-sepolia";
-        networks[84532] = "base-sepolia";
+        _networks[11155111] = "sepolia";
+        _networks[421614] = "arbitrum-sepolia";
+        _networks[84532] = "base-sepolia";
 
-        routers["sepolia"] = RiscZeroVerifierRouter(0x925d8331ddc0a1F0d96E68CF073DFE1d92b69187);
-        routers["arbitrum-sepolia"] = RiscZeroVerifierRouter(0x0b144E07A0826182B6b59788c34b32Bfa86Fb711);
-        routers["base-sepolia"] = RiscZeroVerifierRouter(0x0b144E07A0826182B6b59788c34b32Bfa86Fb711);
+        _routers["sepolia"] = RiscZeroVerifierRouter(0x925d8331ddc0a1F0d96E68CF073DFE1d92b69187);
+        _routers["arbitrum-sepolia"] = RiscZeroVerifierRouter(0x0b144E07A0826182B6b59788c34b32Bfa86Fb711);
+        _routers["base-sepolia"] = RiscZeroVerifierRouter(0x0b144E07A0826182B6b59788c34b32Bfa86Fb711);
     }
 
     function run() public returns (address protocolAdapter) {
         vm.startBroadcast();
         protocolAdapter = address(
             new ProtocolAdapter{salt: sha256("ProtocolAdapterDraft")}({
-                riscZeroVerifierRouter: routers[networks[block.chainid]],
+                riscZeroVerifierRouter: _routers[_networks[block.chainid]],
                 commitmentTreeDepth: 32,
                 actionTagTreeDepth: 4
             })
