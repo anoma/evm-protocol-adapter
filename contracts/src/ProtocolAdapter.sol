39cac361c32ccc5c9e6c526c6d0e434d76a234b0
Artem Gureev <67437331+agureev@users.noreply.github.com>
2025-09-11 20:36:06 +0400

    Revert "Remove Tag Lookup File"

--- contracts/src/ProtocolAdapter.sol
+++ contracts/src/ProtocolAdapter.sol
@@ -10,6 +10,7 @@ import {IProtocolAdapter} from "./interfaces/IProtocolAdapter.sol";
 
 import {MerkleTree} from "./libs/MerkleTree.sol";
 import {RiscZeroUtils} from "./libs/RiscZeroUtils.sol";
+import {TagLookup} from "./libs/TagLookup.sol";
 
 import {Compliance} from "./proving/Compliance.sol";
 import {Delta} from "./proving/Delta.sol";
@@ -28,6 +29,7 @@ string constant PROTOCOL_ADAPTER_VERSION = "1.0.0-beta";
 /// @custom:security-contact security@anoma.foundation
 contract ProtocolAdapter is IProtocolAdapter, ReentrancyGuardTransient, CommitmentAccumulator, NullifierSet {
     using MerkleTree for bytes32[];
+    using TagLookup for bytes32[];
     using RiscZeroUtils for Compliance.Instance;
     using RiscZeroUtils for Logic.VerifierInput;
     using Logic for Logic.VerifierInput[];
