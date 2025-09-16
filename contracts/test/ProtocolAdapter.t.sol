497150e6a6dd0d90c9125a13c8f3bf86fe8ad31d
Michael Heuer <michael.heuer@rwth-aachen.de>
2025-09-16 18:55:08 +0200

    feat: add tests for the empty transaction

--- contracts/test/ProtocolAdapter.t.sol
+++ contracts/test/ProtocolAdapter.t.sol
@@ -6,9 +6,12 @@ import {Pausable} from "@openzeppelin-contracts/utils/Pausable.sol";
 import {RiscZeroVerifierEmergencyStop} from "@risc0-ethereum/RiscZeroVerifierEmergencyStop.sol";
 import {RiscZeroVerifierRouter} from "@risc0-ethereum/RiscZeroVerifierRouter.sol";
 
-import {Test} from "forge-std/Test.sol";
+import {Test, Vm} from "forge-std/Test.sol";
 
+import {ICommitmentAccumulator} from "../src/interfaces/ICommitmentAccumulator.sol";
+import {IProtocolAdapter} from "../src/interfaces/IProtocolAdapter.sol";
 import {ProtocolAdapter} from "../src/ProtocolAdapter.sol";
+import {Transaction, Action} from "../src/Types.sol";
 import {TransactionExample} from "./examples/Transaction.e.sol";
 import {DeployRiscZeroContracts} from "./script/DeployRiscZeroContracts.s.sol";
 
@@ -41,5 +44,27 @@ contract ProtocolAdapterTest is Test {
 
     function test_execute() public {
         _pa.execute(TransactionExample.transaction());
+    }
+
+    function test_execute_executes_the_empty_transaction() public {
+        Transaction memory emptyTx = Transaction({actions: new Action[](0), deltaProof: ""});
+
+        vm.expectEmit(address(_pa));
+        emit IProtocolAdapter.TransactionExecuted({tags: new bytes32[](0), logicRefs: new bytes32[](0)});
+        _pa.execute(emptyTx);
+    }
+
+    function test_execute_does_not_emit_the_CommitmentTreeRootStored_event_for_the_empty_transaction() public {
+        Transaction memory emptyTx = Transaction({actions: new Action[](0), deltaProof: ""});
+
+        vm.recordLogs();
+
+        _pa.execute(emptyTx);
+
+        Vm.Log[] memory entries = vm.getRecordedLogs();
+
+        for (uint256 i = 0; i < entries.length; i++) {
+            assert(entries[i].topics[0] != ICommitmentAccumulator.CommitmentTreeRootStored.selector);
+        }
     }
 }
