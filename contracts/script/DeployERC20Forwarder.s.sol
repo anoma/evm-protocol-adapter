950300bd3cc6f20cea80affa38415da8871570c3
Michael Heuer <michael.heuer@rwth-aachen.de>
2025-09-17 18:57:13 +0200

    refactor: use random salt for test deployments

--- contracts/script/DeployERC20Forwarder.s.sol
+++ contracts/script/DeployERC20Forwarder.s.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.30;
 
 import {Script} from "forge-std/Script.sol";
 import {ERC20Forwarder} from "../src/forwarders/ERC20Forwarder.sol";
-import {ProtocolAdapter} from "../src/ProtocolAdapter.sol";
+import {ProtocolAdapter, PROTOCOL_ADAPTER_VERSION} from "../src/ProtocolAdapter.sol";
 
 contract DeployERC20Forwarder is Script {
     ProtocolAdapter internal constant _PROTOCOL_ADAPTER = ProtocolAdapter(address(0));
@@ -12,9 +12,16 @@ contract DeployERC20Forwarder is Script {
 
     bytes32 internal constant _CALLDATA_CARRIER_LOGIC_REF = bytes32(0);
 
-    function run() public {
+    function run(bool isTestDeployment) public {
+        bytes32 salt;
+        if (isTestDeployment) {
+            salt = bytes32(block.prevrandao);
+        } else {
+            salt = keccak256(bytes(string.concat("ERC20Forwarder", " ", PROTOCOL_ADAPTER_VERSION)));
+        }
+
         vm.startBroadcast();
-        new ERC20Forwarder{salt: sha256("ERC20ForwarderExample")}({
+        new ERC20Forwarder{salt: salt}({
             protocolAdapter: address(_PROTOCOL_ADAPTER),
             emergencyCommittee: _EMERGENCY_COMMITTEE,
             calldataCarrierLogicRef: _CALLDATA_CARRIER_LOGIC_REF
