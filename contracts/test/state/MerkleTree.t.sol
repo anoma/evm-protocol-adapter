44b056c10e921bb87bfd1fc52d08ec4df3870feb
agureev <artem@heliax.dev>
2025-09-21 16:46:48 +0400

    Change "leaves" Spelling

--- contracts/test/state/MerkleTree.t.sol
+++ contracts/test/state/MerkleTree.t.sol
@@ -22,31 +22,31 @@ contract MerkleTreeTest is Test, MerkleTreeExample {
         assertEq(_merkleTree.leafCount(), 0);
         assertEq(_merkleTree.depth(), 0);
 
-        _merkleTree.push(_leafs[7][0]);
+        _merkleTree.push(_leaves[7][0]);
         assertEq(_merkleTree.leafCount(), 1);
         assertEq(_merkleTree.depth(), 1);
 
-        _merkleTree.push(_leafs[7][1]);
+        _merkleTree.push(_leaves[7][1]);
         assertEq(_merkleTree.leafCount(), 2);
         assertEq(_merkleTree.depth(), 2);
 
-        _merkleTree.push(_leafs[7][2]);
+        _merkleTree.push(_leaves[7][2]);
         assertEq(_merkleTree.leafCount(), 3);
         assertEq(_merkleTree.depth(), 2);
 
-        _merkleTree.push(_leafs[7][3]);
+        _merkleTree.push(_leaves[7][3]);
         assertEq(_merkleTree.leafCount(), 4);
         assertEq(_merkleTree.depth(), 3);
 
-        _merkleTree.push(_leafs[7][4]);
+        _merkleTree.push(_leaves[7][4]);
         assertEq(_merkleTree.leafCount(), 5);
         assertEq(_merkleTree.depth(), 3);
 
-        _merkleTree.push(_leafs[7][5]);
+        _merkleTree.push(_leaves[7][5]);
         assertEq(_merkleTree.leafCount(), 6);
         assertEq(_merkleTree.depth(), 3);
 
-        _merkleTree.push(_leafs[7][6]);
+        _merkleTree.push(_leaves[7][6]);
         assertEq(_merkleTree.leafCount(), 7);
         assertEq(_merkleTree.depth(), 3);
     }
