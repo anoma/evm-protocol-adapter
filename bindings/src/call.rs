55142ba1af3be03eb4d86c86e6cfe3dbe6e42805
Murisi Tarusenga <murisit@gmail.com>
2025-09-26 16:27:45 +0200

    Added some documentation.

--- bindings/src/call.rs
+++ bindings/src/call.rs
@@ -2,36 +2,30 @@ use crate::conversion;
 
 use alloy::network::EthereumWallet;
 use alloy::primitives::{Address, U256};
-use alloy::providers::fillers::{
-    BlobGasFiller, ChainIdFiller, FillProvider, GasFiller, JoinFill, NonceFiller, WalletFiller,
-};
-use alloy::providers::{Identity, ProviderBuilder, RootProvider};
-use alloy::sol;
+use alloy::providers::ProviderBuilder;
 use reqwest::Url;
 use url::ParseError;
 use arm_risc0::transaction::Transaction;
 use async_trait::async_trait;
 
-type DefaultProvider = FillProvider<
-    JoinFill<
-        JoinFill<
-            Identity,
-            JoinFill<GasFiller, JoinFill<BlobGasFiller, JoinFill<NonceFiller, ChainIdFiller>>>,
-        >,
-        WalletFiller<EthereumWallet>,
-    >,
-    RootProvider,
-    >;
-
+/// The interface exposed by compliant protocol adapters
 #[async_trait]
 pub trait ProtocolAdapter {
+    /// Execute the given transaction
     async fn execute(&self, tx: Transaction) -> Result<(), alloy::contract::Error>;
+    /// Stop the protocol adapter permanently
     async fn emergency_stop(&self) -> Result<(), alloy::contract::Error>;
+    /// Check if the protocol adapter has been stopped or not
     async fn is_emergency_stopped(&self) -> Result<bool, alloy::contract::Error>;
+    /// Get the RISC Zero verifier selector
     async fn get_risc_zero_verifier_selector(&self) -> Result<[u8; 4], alloy::contract::Error>;
+    /// Get the protocol adapter version
     async fn get_protocol_adapter_version(&self) -> Result<[u8; 32], alloy::contract::Error>;
+    /// Get the latest commitment tree state root
     async fn latest_root(&self) -> Result<[u8; 32], alloy::contract::Error>;
+    /// Check if the commitment tree state root exists
     async fn contains_root(&self, root: &[u8; 32]) -> Result<bool, alloy::contract::Error>;
+    ///Verifies that a Merkle path and a commitment leaf reproduce a given root
     async fn verify_merkle_proof(
         &self,
         root: &[u8; 32],
@@ -39,27 +33,39 @@ pub trait ProtocolAdapter {
         path: &[[u8; 32]],
         direction_bits: U256,
     ) -> Result<(), alloy::contract::Error>;
+    /// Check if the nullifier set contains the given nullifier
     async fn contains(&self, nullifier: &[u8; 32]) -> Result<bool, alloy::contract::Error>;
+    /// Get the length of the nullifier set
     async fn length(&self) -> Result<U256, alloy::contract::Error>;
+    /// Get the nullifier at the given index in the set
     async fn at_index(&self, index: U256) -> Result<[u8; 32], alloy::contract::Error>;
 }
 
-
+/// An Ethereum RPC endpoint
 pub enum EthereumRpc {
+    /// A Sepolia RPC endpoint
     Sepolia(Url),
-    Other(Url),
+    /// A custom Ethereum RPC endpoint
+    Custom(Url),
 }
 
 impl EthereumRpc {
+    /// A general Sepolia RPC endpoint
     pub fn sepolia_rpc(rpc_url: String) -> Result<Self, ParseError> {
         Ok(Self::Sepolia(rpc_url.parse()?))
     }
-    
+
+    /// An Alchemy Sepolia RPC endpoint with the given API key
     pub fn alchemy_sepolia_rpc(api_key: String) -> Result<Self, ParseError> {
         Ok(Self::Sepolia(format!(
             "https://eth-sepolia.g.alchemy.com/v2/{}",
             api_key
         ).parse()?))
+    }
+
+    /// A custom RPC endpoint
+    pub fn custom_rpc(rpc_url: String) -> Result<Self, ParseError> {
+        Ok(Self::Custom(rpc_url.parse()?))
     }
 }
 
@@ -67,16 +73,21 @@ impl From<EthereumRpc> for Url {
     fn from(rpc: EthereumRpc) -> Self {
         match rpc {
             EthereumRpc::Sepolia(url) => url,
-            EthereumRpc::Other(url) => url,
+            EthereumRpc::Custom(url) => url,
         }
     }
 }
 
+/// Uniquely identifies a protocol adapter
 pub enum ProtocolAdapterId {
+    /// The adapter deployed on Sepolia
     SepoliaPa,
-    Other { protocol_adapter: Address, erc20_forwarder: Address },
+    /// Some other instance deployed at the given address
+    Custom { protocol_adapter: Address },
 }
 
+/// Construct an endpoint for the identified protocol adapter over the given RPC
+/// and wallet
 pub fn protocol_adapter(
     id: ProtocolAdapterId,
     rpc: EthereumRpc,
@@ -84,48 +95,21 @@ pub fn protocol_adapter(
 ) -> Box<dyn ProtocolAdapter> {
     let provider = ProviderBuilder::new().wallet(wallet);
     match (id, rpc) {
+        // Sepolia RPC must be used with the Sepolia protocol adapter instance
         (ProtocolAdapterId::SepoliaPa, EthereumRpc::Sepolia(rpc_url)) => {
+            // The address of the Sepolia Protocol Adapter instance
             let protocol_adapter_address = "0xaf21c8a4d489610f42aabc883e66be3d651e5d52"
                 .parse::<Address>()
                 .expect("Sepolia deployment address should be correct");
             let provider = provider.connect_http(rpc_url);
             Box::new(conversion::ProtocolAdapter::new(protocol_adapter_address, provider))
         },
-        (ProtocolAdapterId::Other { protocol_adapter, .. }, rpc) => {
+        // Custom protocol adapter instances can be used with any RPC
+        (ProtocolAdapterId::Custom { protocol_adapter, .. }, rpc) => {
             let provider = provider.connect_http(rpc.into());
             Box::new(conversion::ProtocolAdapter::new(protocol_adapter, provider))
         },
         _ => unreachable!("Incompatable RPC chosen for protocol adapter"),
-    }
-}
-
-sol!(
-    #[allow(missing_docs)]
-    #[derive(Debug, PartialEq, serde::Serialize, serde::Deserialize)]
-    #[sol(rpc)]
-    ERC20Forwarder,
-    "../contracts/out/ERC20Forwarder.sol/ERC20Forwarder.json"
-);
-
-pub fn erc20_forwarder(
-    id: ProtocolAdapterId,
-    rpc: EthereumRpc,
-    wallet: EthereumWallet,
-) -> ERC20Forwarder::ERC20ForwarderInstance<DefaultProvider> {
-    let provider = ProviderBuilder::new().wallet(wallet);
-    match (id, rpc) {
-        (ProtocolAdapterId::SepoliaPa, EthereumRpc::Sepolia(rpc_url)) => {
-            let erc20_forwarder_address = "0xaf21c8a4d489610f42aabc883e66be3d651e5d52"
-                .parse::<Address>()
-                .expect("Wrong address format");
-            let provider = provider.connect_http(rpc_url);
-            ERC20Forwarder::new(erc20_forwarder_address, provider)
-        },
-        (ProtocolAdapterId::Other { erc20_forwarder, .. }, rpc) => {
-            let provider = provider.connect_http(rpc.into());
-            ERC20Forwarder::new(erc20_forwarder, provider)
-        },
-        _ => unreachable!("Incompatable RPC chosen for ERC-20 forwarder"),
     }
 }
 
