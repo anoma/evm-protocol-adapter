71ccb3bf64a20dfcc0a330cfe0272fe686a64b6f
Michael Heuer <michael.heuer@rwth-aachen.de>
2025-10-01 18:30:13 +0200

    style: fix import

--- contracts/src/state/CommitmentTree.sol
+++ contracts/src/state/CommitmentTree.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.30;
 
-import {Arrays} from "@openzeppelin-contracts/utils/Arrays.sol";
 import {EnumerableSet} from "@openzeppelin-contracts/utils/structs/EnumerableSet.sol";
 
 import {ICommitmentTree} from "../interfaces/ICommitmentTree.sol";
