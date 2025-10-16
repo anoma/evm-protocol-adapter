5409ac8093e75e2896028affba4a826c14fb57b2
Michael Heuer <michael.heuer@rwth-aachen.de>
2025-10-16 20:36:22 +0200

    feat: add tests

--- bindings/src/call.rs
+++ bindings/src/call.rs
@@ -1,96 +1,74 @@
-use crate::abi::ProtocolAdapter;
+extern crate dotenv;
 
-use alloy::network::EthereumWallet;
 use alloy::primitives::{address, Address};
-use alloy::providers::fillers::{
-    BlobGasFiller, ChainIdFiller, FillProvider, GasFiller, JoinFill, NonceFiller, WalletFiller,
-};
-use alloy::providers::{Identity, ProviderBuilder, RootProvider};
-use alloy::signers::local::PrivateKeySigner;
-use std::env;
-
-type DefaultProvider = FillProvider<
-    JoinFill<
-        JoinFill<
-            Identity,
-            JoinFill<GasFiller, JoinFill<BlobGasFiller, JoinFill<NonceFiller, ChainIdFiller>>>,
-        >,
-        WalletFiller<EthereumWallet>,
-    >,
-    RootProvider,
->;
 
 pub const PROTOCOL_ADAPTER_ADDRESS_SEPOLIA: Address =
-    address!("0x375920798465eb6b845AC5BF8580d69ce0Bda34a");
-
-pub fn protocol_adapter() -> ProtocolAdapter::ProtocolAdapterInstance<DefaultProvider> {
-    ProtocolAdapter::new(PROTOCOL_ADAPTER_ADDRESS_SEPOLIA, provider())
-}
-
-pub fn provider() -> DefaultProvider {
-    let signer = env::var("PRIVATE_KEY")
-        .expect("Couldn't read PRIVATE_KEY")
-        .parse::<PrivateKeySigner>()
-        .expect("Wrong private key format");
-
-    let rpc_url = format!(
-        "https://eth-sepolia.g.alchemy.com/v2/{}",
-        env::var("API_KEY_ALCHEMY").expect("Couldn't read API_KEY_ALCHEMY")
-    );
-
-    let wallet: EthereumWallet = signer.into();
-
-    ProviderBuilder::new()
-        .wallet(wallet)
-        .connect_http(rpc_url.parse().expect("Failed to parse RPC URL"))
-}
+    address!("0xfF91D5653b7121718DE6BE553ef7014EF131EF50");
 
 #[cfg(test)]
 mod tests {
     use crate::abi::ProtocolAdapter;
-    use crate::call::protocol_adapter;
-    use alloy::hex;
+    use crate::abi::ProtocolAdapter::ProtocolAdapterInstance;
+    use crate::call::PROTOCOL_ADAPTER_ADDRESS_SEPOLIA;
     use alloy::primitives::B256;
+    use alloy::providers::{Provider, ProviderBuilder};
+    use alloy::signers::local::PrivateKeySigner;
+    use std::env;
     use tokio;
 
-    fn initial_root() -> B256 {
-        B256::from(hex!(
-            "7e70786b1d52fc0412d75203ef2ac22de13d9596ace8a5a1ed5324c3ed7f31c3"
-        ))
-    }
-
     #[tokio::test]
-    #[ignore = "This test requires updatng the protocol adapter address in .env"]
-    async fn contains_initial_root() {
-        assert!(
-            protocol_adapter()
-                .isCommitmentTreeRootContained(initial_root())
-                .call()
-                .await
-                .unwrap()
+    async fn protocol_adapter_version_matches_the_cargo_pkg_version() {
+        let pa_version = pa_instance()
+            .getProtocolAdapterVersion()
+            .call()
+            .await
+            .unwrap();
+
+        assert_eq!(
+            decode_bytes32_to_utf8(pa_version),
+            env!("CARGO_PKG_VERSION").to_string()
         );
     }
 
     #[tokio::test]
-    #[ignore = "This test requires updating the protocol adapter address in .env"]
-    async fn call_latest_root() {
-        let root = protocol_adapter()
-            .latestCommitmentTreeRoot()
-            .call()
-            .await
-            .unwrap();
-        assert_ne!(root, initial_root());
-    }
-
-    #[tokio::test]
-    #[ignore = "This test requires updating the protocol adapter address in .env"]
-    async fn call_execute() {
+    async fn call_executes_the_empty_tx() {
         let empty_tx = ProtocolAdapter::Transaction {
             actions: vec![],
             aggregationProof: vec![].into(),
             deltaProof: vec![].into(),
         };
-        let result = protocol_adapter().execute(empty_tx).call().await;
-        assert!(result.is_ok());
+
+        let receipt = pa_instance()
+            .execute(empty_tx)
+            .send()
+            .await
+            .unwrap()
+            .get_receipt()
+            .await
+            .unwrap();
+
+        assert!(receipt.inner.is_success());
+    }
+
+    fn pa_instance() -> ProtocolAdapterInstance<impl Provider> {
+        dotenv::dotenv().ok();
+        let url = format!(
+            "https://eth-sepolia.g.alchemy.com/v2/{}",
+            env::var("API_KEY_ALCHEMY").expect("Couldn't read API_KEY_ALCHEMY")
+        );
+
+        let signer: PrivateKeySigner = env::var("PRIVATE_KEY").unwrap().parse().unwrap();
+
+        let provider = ProviderBuilder::new()
+            .wallet(signer)
+            .connect_anvil_with_config(|a| a.fork(url));
+        ProtocolAdapter::new(PROTOCOL_ADAPTER_ADDRESS_SEPOLIA, provider)
+    }
+
+    fn decode_bytes32_to_utf8(encoded_string: B256) -> String {
+        let bytes = alloy::hex::decode(encoded_string.to_string()).unwrap();
+
+        let trimmed = bytes.split(|b| *b == 0).next().unwrap();
+        str::from_utf8(trimmed).unwrap().to_string()
     }
 }
