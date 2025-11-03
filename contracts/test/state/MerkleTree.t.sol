29671f9d9889f1ab6e6169febbeb5b98248d710c
Michael Heuer <michael.heuer@rwth-aachen.de>
2025-11-03 15:05:44 +0100

    refactor: optimize Merkle tree depth computation

--- contracts/test/state/MerkleTree.t.sol
+++ contracts/test/state/MerkleTree.t.sol
@@ -111,11 +111,115 @@ contract MerkleTreeTest is Test, MerkleTreeExample {
         testFuzz_push_returns_the_same_roots(dynamicLeaves);
     }
 
+    function test_compare_le() public pure {
+        assertEq(_computeMinimalTreeDepthNaive(0), MerkleTree.computeMinimalTreeDepth(0));
+        assertEq(MerkleTree.computeMinimalTreeDepth(0), 0);
+    }
+
+    function test_computeMinimalTreeDepth_computes_the_right_tree_depths() public pure {
+        uint8[34] memory depths =
+            [0, 0, 1, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 6];
+
+        for (uint256 i = 0; i < depths.length; ++i) {
+            assertEq(MerkleTree.computeMinimalTreeDepth({leavesCount: i}), depths[i]);
+        }
+    }
+
+    function test_computeMinimalTreeDepth_reproduces_tree_depths_of_the_naive_implementation() public pure {
+        uint256 maxLeafCount = 16;
+
+        for (uint256 i = 0; i < maxLeafCount; ++i) {
+            assertEq(
+                _computeMinimalTreeDepthNaive({leavesCount: i}), MerkleTree.computeMinimalTreeDepth({leavesCount: i})
+            );
+        }
+    }
+
+    function test_compare_power_of_two_boundaries() public pure {
+        // Test powers of 2 and their neighbors
+        uint256[10] memory testCases = [uint256(1), 2, 4, 8, 16, 32, 64, 128, 256, 512];
+
+        for (uint256 i = 0; i < testCases.length; i++) {
+            uint256 powerOfTwo = testCases[i];
+
+            assertEq(_computeMinimalTreeDepthNaive(powerOfTwo - 1), MerkleTree.computeMinimalTreeDepth(powerOfTwo - 1));
+
+            // Test power of 2
+            assertEq(_computeMinimalTreeDepthNaive(powerOfTwo), MerkleTree.computeMinimalTreeDepth(powerOfTwo));
+
+            // Test power of 2 + 1
+            assertEq(_computeMinimalTreeDepthNaive(powerOfTwo + 1), MerkleTree.computeMinimalTreeDepth(powerOfTwo + 1));
+        }
+    }
+
+    function test_compare_large_values() public pure {
+        // Test some large but realistic values
+        uint256[5] memory testCases = [uint256(1000), 10000, 100000, 1000000, 10000000];
+
+        for (uint256 i = 0; i < testCases.length; i++) {
+            assertEq(_computeMinimalTreeDepthNaive(testCases[i]), MerkleTree.computeMinimalTreeDepth(testCases[i]));
+        }
+    }
+
+    /// @notice Fuzz test with unbounded uint256
+    function testFuzz_compare_implementations_unbounded(uint256 leavesCount) public pure {
+        leavesCount = bound(leavesCount, 0, type(uint128).max);
+
+        uint8 original = _computeMinimalTreeDepthNaive(leavesCount);
+        uint8 optimized = MerkleTree.computeMinimalTreeDepth(leavesCount);
+
+        assertEq(original, optimized, "Implementations must match");
+    }
+
+    /// @notice Fuzz test with small values (0-1000)
+    function testFuzz_compare_implementations_small(uint256 leavesCount) public pure {
+        leavesCount = bound(leavesCount, 0, 1000);
+
+        uint8 original = _computeMinimalTreeDepthNaive(leavesCount);
+        uint8 optimized = MerkleTree.computeMinimalTreeDepth(leavesCount);
+
+        assertEq(original, optimized, "Implementations must match");
+    }
+
+    /// @notice Fuzz test with medium values (1000-1000000)
+    function testFuzz_compare_implementations_medium(uint256 leavesCount) public pure {
+        leavesCount = bound(leavesCount, 1000, 1000000);
+
+        uint8 original = _computeMinimalTreeDepthNaive(leavesCount);
+        uint8 optimized = MerkleTree.computeMinimalTreeDepth(leavesCount);
+
+        assertEq(original, optimized, "Implementations must match");
+    }
+
+    function testFuzz_gas_naive(uint256 leavesCount) public pure {
+        leavesCount = bound(leavesCount, 0, 1000000);
+        _computeMinimalTreeDepthNaive(leavesCount);
+    }
+
+    /// @notice Fuzz test - Optimized implementation only (to measure gas)
+    function testFuzz_gas_optimized(uint256 leavesCount) public pure {
+        leavesCount = bound(leavesCount, 0, 1000000);
+        MerkleTree.computeMinimalTreeDepth(leavesCount);
+    }
+
     /// @notice Hashes two `bytes32` values.
     /// @param a The first value to hash.
     /// @param b The second value to hash.
     /// @return hab The resulting hash.
     function _hashPair(bytes32 a, bytes32 b) internal pure returns (bytes32 hab) {
         hab = SHA256.hash(a, b);
+    }
+
+    /// @notice The naive implementation using a loop.
+    function _computeMinimalTreeDepthNaive(uint256 leavesCount) internal pure returns (uint8 treeDepth) {
+        uint256 treeCapacity = 1;
+        treeDepth = 0;
+
+        while (treeCapacity < leavesCount) {
+            treeCapacity *= 2;
+            ++treeDepth;
+        }
+
+        return treeDepth;
     }
 }
