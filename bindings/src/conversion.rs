fdd35e7dd44d0cb561767e08252de6021be66cb5
Michael Heuer <michael.heuer@rwth-aachen.de>
2025-11-14 18:12:09 +0100

    refactor: move tests

--- bindings/src/conversion.rs
+++ bindings/src/conversion.rs
@@ -9,7 +9,7 @@ use arm_risc0::proving_system::encode_seal;
 use arm_risc0::transaction::{Delta as ArmDelta, Transaction};
 use arm_risc0::utils::words_to_bytes;
 
-use crate::contract::{Compliance, Logic, ProtocolAdapter};
+pub use crate::contract::{Compliance, Logic, ProtocolAdapter};
 
 impl From<ExpirableBlob> for Logic::ExpirableBlob {
     fn from(expirable_blob: ExpirableBlob) -> Self {
@@ -130,82 +130,5 @@ impl From<Transaction> for ProtocolAdapter::Transaction {
                 None => Bytes::from(""),
             },
         }
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use crate::conversion::ProtocolAdapter;
-    use alloy::primitives::B256;
-    use alloy::sol_types::SolValue;
-    use arm_risc0::aggregation::AggregationStrategy;
-
-    #[test]
-    fn print_verifying_keys() {
-        println!(
-            "COMPLIANCE_VK: {:?}",
-            B256::from_slice(arm_risc0::constants::COMPLIANCE_VK.as_bytes())
-        );
-
-        println!(
-            "BATCH_AGGREGATION_VK: {:?}",
-            B256::from_slice(arm_risc0::aggregation::constants::BATCH_AGGREGATION_VK.as_bytes())
-        );
-    }
-
-    #[test]
-    #[ignore]
-    fn generate_tx_reg() {
-        let n_actions = 2;
-        let n_cus = 2;
-
-        let tx = arm_risc0::tests::generate_test_transaction(n_actions, n_cus);
-
-        to_evm_bin_file(
-            ProtocolAdapter::Transaction::from(tx),
-            "tx_reg",
-            &n_actions,
-            &n_cus,
-        );
-    }
-
-    #[test]
-    #[ignore]
-    fn generate_tx_agg() {
-        let n_actions = 1;
-        let n_cus = 1;
-
-        let mut tx = arm_risc0::tests::generate_test_transaction(n_actions, n_cus);
-
-        tx.aggregate_with_strategy(AggregationStrategy::Batch)
-            .unwrap();
-
-        to_evm_bin_file(
-            ProtocolAdapter::Transaction::from(tx),
-            "tx_agg",
-            &n_actions,
-            &n_cus,
-        );
-    }
-
-    fn to_evm_bin_file(
-        tx: ProtocolAdapter::Transaction,
-        name: &str,
-        n_actions: &usize,
-        n_cus: &usize,
-    ) {
-        let encoded_tx = tx.abi_encode();
-        let decoded_tx: ProtocolAdapter::Transaction =
-            ProtocolAdapter::Transaction::abi_decode(&encoded_tx).unwrap();
-        assert_eq!(tx, decoded_tx);
-
-        println!("Transaction: {tx:#?}");
-        std::fs::write(
-            format!(
-                "../contracts/test/examples/transactions/test_{name}_{n_actions:02}_{n_cus:02}.bin"
-            ),
-            &encoded_tx,
-        )
-        .expect("Failed to write encoded transaction to file");
     }
 }
