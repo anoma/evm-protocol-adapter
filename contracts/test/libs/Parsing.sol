1050b78e4e2c5ed3285babdd2337b4e7ca99a463
Michael Heuer <michael.heuer@rwth-aachen.de>
2025-11-18 11:51:53 +0100

    feat: add example transaction execution script

--- contracts/test/libs/Parsing.sol
+++ contracts/test/libs/Parsing.sol
@@ -7,7 +7,7 @@ import {Transaction} from "../../src/Types.sol";
 
 library Parsing {
     function parseTransaction(Vm vm, string memory path) internal view returns (Transaction memory txn) {
-        string memory fullPath = string.concat(vm.projectRoot(), path);
+        string memory fullPath = string.concat(vm.projectRoot(), "/", path);
         bytes memory data = vm.readFileBinary(fullPath);
 
         txn = abi.decode(data, (Transaction));
