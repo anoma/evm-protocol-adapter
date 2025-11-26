024688f25332ac5bd02303697e59c865c11992db
Michael Heuer <michael.heuer@rwth-aachen.de>
2025-11-26 17:37:39 +0100

    feat: deployed on sepolia

--- bindings/src/addresses.rs
+++ bindings/src/addresses.rs
@@ -5,16 +5,10 @@ use std::collections::HashMap;
 /// Returns a map of protocol adapter deployments for all supported chains.
 pub fn protocol_adapter_deployments_map() -> HashMap<NamedChain, Address> {
     use NamedChain::*;
-    HashMap::from([
-        (
-            Sepolia,
-            address!("0x0b74B3f295344Ae4e79b94Fb02c7d640dFF6176E"),
-        ),
-        (
-            Mainnet,
-            address!("0x46E622226F93Ed52C584F3f66135CD06AF01c86c"),
-        ),
-    ])
+    HashMap::from([(
+        Sepolia,
+        address!("0x71b36D618e8B086EBbE0896c5214C8464B1dFDD4"),
+    )])
 }
 
 /// Returns the address of the protocol adapter deployed on the provided chain, if any.
