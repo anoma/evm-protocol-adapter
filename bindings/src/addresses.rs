c6bf83b38d485352700ac9465dc7ac99986cedba
Michael <20623991+heueristik@users.noreply.github.com>
2025-12-05 15:41:55 +0100

    chore: release `bindings` `1.0.0-rc.8` (#427)
    * feat: deployed on base-sepolia
    
    * feat: deployed on base
    
    * feat: deployed on optimism-sepolia
    
    * feat: deployed on optimism
    
    * chore: bump bindings version

--- bindings/src/addresses.rs
+++ bindings/src/addresses.rs
@@ -22,6 +22,19 @@ pub fn protocol_adapter_deployments_map() -> HashMap<NamedChain, Address> {
             Arbitrum,
             address!("0x6d0A05E3535bd4D2C32AaD37FFB28fd0E1e528c3"),
         ),
+        (
+            BaseSepolia,
+            address!("0x6d0A05E3535bd4D2C32AaD37FFB28fd0E1e528c3"),
+        ),
+        (Base, address!("0x6d0A05E3535bd4D2C32AaD37FFB28fd0E1e528c3")),
+        (
+            OptimismSepolia,
+            address!("0x3a182dcCC17Dd68407cFbc9257fF88349F5153fd"),
+        ),
+        (
+            Optimism,
+            address!("0x6d0A05E3535bd4D2C32AaD37FFB28fd0E1e528c3"),
+        ),
     ])
 }
 
