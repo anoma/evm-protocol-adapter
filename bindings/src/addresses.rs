d20edc5906440378bdc96c656836ec67f2f4fdbd
Michael Heuer <michael.heuer@rwth-aachen.de>
2025-12-16 09:11:17 +0100

    chore: update addresses

--- bindings/src/addresses.rs
+++ bindings/src/addresses.rs
@@ -5,37 +5,7 @@ use std::collections::HashMap;
 /// Returns a map of protocol adapter deployments for all supported chains.
 pub fn protocol_adapter_deployments_map() -> HashMap<NamedChain, Address> {
     use NamedChain::*;
-    HashMap::from([
-        (
-            Sepolia,
-            address!("0x71b36D618e8B086EBbE0896c5214C8464B1dFDD4"),
-        ),
-        (
-            Mainnet,
-            address!("0xf031F3c63400417ADA22B4D43b7bAa5E699548B1"),
-        ),
-        (
-            ArbitrumSepolia,
-            address!("0x6d0A05E3535bd4D2C32AaD37FFB28fd0E1e528c3"),
-        ),
-        (
-            Arbitrum,
-            address!("0x6d0A05E3535bd4D2C32AaD37FFB28fd0E1e528c3"),
-        ),
-        (
-            BaseSepolia,
-            address!("0x6d0A05E3535bd4D2C32AaD37FFB28fd0E1e528c3"),
-        ),
-        (Base, address!("0x6d0A05E3535bd4D2C32AaD37FFB28fd0E1e528c3")),
-        (
-            OptimismSepolia,
-            address!("0x3a182dcCC17Dd68407cFbc9257fF88349F5153fd"),
-        ),
-        (
-            Optimism,
-            address!("0x6d0A05E3535bd4D2C32AaD37FFB28fd0E1e528c3"),
-        ),
-    ])
+    HashMap::from([])
 }
 
 /// Returns the address of the protocol adapter deployed on the provided chain, if any.
