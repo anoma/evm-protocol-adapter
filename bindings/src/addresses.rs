8c3881b9984fb586946b2f68923e9d5824ea7538
Michael Heuer <michael.heuer@rwth-aachen.de>
2026-01-12 22:34:15 +0100

    feat: add test deployment address

--- bindings/src/addresses.rs
+++ bindings/src/addresses.rs
@@ -1,10 +1,13 @@
-use alloy::primitives::Address;
+use alloy::primitives::{Address, address};
 use alloy_chains::NamedChain;
 use std::collections::HashMap;
 
 /// Returns a map of protocol adapter deployments for all supported chains.
 pub fn protocol_adapter_deployments_map() -> HashMap<NamedChain, Address> {
-    HashMap::from([])
+    HashMap::from([(
+        NamedChain::Sepolia,
+        address!("0xbCeae10603308bd0Aa6781F15F3A0E0A406356FD"), // Test deployment
+    )])
 }
 
 /// Returns the address of the protocol adapter deployed on the provided chain, if any.
