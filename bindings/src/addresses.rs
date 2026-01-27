c702fd389ca3d78c931d16ced5cbfbb7e92abf3e
Michael Heuer <michael.heuer@rwth-aachen.de>
2026-01-27 12:30:46 +0100

    chore: bump version numbers

--- bindings/src/addresses.rs
+++ bindings/src/addresses.rs
@@ -7,27 +7,27 @@ pub fn protocol_adapter_deployments_map() -> HashMap<NamedChain, Address> {
     HashMap::from([
         (
             NamedChain::Sepolia,
-            address!("0xc63336a48D0f60faD70ed027dFB256908bBD5e37"),
+            address!("0x0000000000000000000000000000000000000000"),
         ),
         (
             NamedChain::Mainnet,
-            address!("0xdd4f4F0875Da48EF6d8F32ACB890EC81F435Ff3a"),
+            address!("0x0000000000000000000000000000000000000000"),
         ),
         (
             NamedChain::BaseSepolia,
-            address!("0x212f275c6dD4829cd84ABDF767b0Df4A9CB9ef60"),
+            address!("0x0000000000000000000000000000000000000000"),
         ),
         (
             NamedChain::Base,
-            address!("0x212f275c6dD4829cd84ABDF767b0Df4A9CB9ef60"),
+            address!("0x0000000000000000000000000000000000000000"),
         ),
         (
             NamedChain::Optimism,
-            address!("0x212f275c6dD4829cd84ABDF767b0Df4A9CB9ef60"),
+            address!("0x0000000000000000000000000000000000000000"),
         ),
         (
             NamedChain::Arbitrum,
-            address!("0x212f275c6dD4829cd84ABDF767b0Df4A9CB9ef60"),
+            address!("0x0000000000000000000000000000000000000000"),
         ),
     ])
 }
