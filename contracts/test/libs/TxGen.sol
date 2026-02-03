0cd02cb0d8070c5c9b8c16bbaeb4932c0cc7f123
Jonathan Cubides <prieto.jona@gmail.com>
2026-02-03 16:39:26 -0500

    refactor(Delta): replace if-revert with require for point validation and address comparison

--- contracts/test/libs/TxGen.sol
+++ contracts/test/libs/TxGen.sol
@@ -373,8 +373,9 @@ library TxGen {
             uint256 complianceUnitCount = actions[i].complianceVerifierInputs.length;
 
             for (uint256 j = 0; j < complianceUnitCount; ++j) {
-                logicRefs[n++] = actions[i].complianceVerifierInputs[j].instance.consumed.logicRef;
-                logicRefs[n++] = actions[i].complianceVerifierInputs[j].instance.created.logicRef;
+                Compliance.Instance memory inst = actions[i].complianceVerifierInputs[j].instance;
+                logicRefs[n++] = inst.consumed.logicRef;
+                logicRefs[n++] = inst.created.logicRef;
             }
         }
     }
